aabb7a573123e581e20d64e04e7facc79f4fd9b7
Abibatouu <fayeaby120@gmail.com>
2025-01-21 14:19:53 +0000

    creation de la base de donnee avec les migrations

--- database/migrations/2025_01_21_123707_create_users_table.php
+++ database/migrations/2025_01_21_123707_create_users_table.php
@@ -9,18 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
+    if (!Schema::hasTable('users')) {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number');
+            $table->string('role');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
+}
+
 
     /**
      * Reverse the migrations.
